01b9028831a2c2b76cd7553d81b45cae77aa6dc3
RStone <358713420@qq.com>
2015-11-03 20:41:15 +0800

    the last

--- getHotBookName.py
+++ getHotBookName.py
@@ -1,8 +1,9 @@
 #-*- coding: utf-8 -*-
+__author__ = 'RStone'
+
 import re
 import urllib
 import requests
-# import chardet
 
 #下载网页
 def getHtml(url):
@@ -11,17 +12,34 @@ def getHtml(url):
     # html = requests.get(url)
     return html
 
-
 #获取书名
 def getName(html):
     bookName = re.findall('<div class="title"><a href="/ebook/.*? &#39;tag&#39;}, true, \'read.douban.com\'\)\">(.*?)</a><',html,re.S)
     return bookName
 
-
 #获取作者
 def getAuthor(html):
     bookAuthor = re.findall('>作者</span><span class="labeled-text"><a class="author-item" href=".*?">(.*?)</a></span></span>',html,re.S)
     return bookAuthor
+
+#获取译者
+def getTranslator(html):
+    extent = re.findall('作者.*?</a></span>(.*?)类别',html,re.S)
+    translatorList = []
+    for matchExist in extent:
+        if re.search("译者",matchExist):
+            translator = re.findall('<span class="labeled-text"><a class="author-item" href=".*?">(.*?)</a>',matchExist,re.S)
+        else:
+            translator = '无译者'.decode('utf8')
+        translatorList.append(translator)
+        continue
+    return translatorList
+
+
+#将作者替换，方便获取译者
+def replaceAuthor(html):
+    replace = re.sub('>作者</span><span class="labeled-text"><a class="author-item" href=".*?</a></span></span>','123',html)
+    return replace
 
 
 #获取图书类别
@@ -38,6 +56,7 @@ def getIntroduction(html):
 
 #获取封面
 def getCover(html,bookName,j = 0):
+    i = 0
     urlOfPic = re.findall('<img width="110px" height="164px" src="(.*?)" alt="" itemprop="image"></a></div>',html,re.S)
     for name in bookName:
         name = "、"+name + ".jpg"
@@ -45,6 +64,7 @@ def getCover(html,bookName,j = 0):
         print 'downloading..' + name.decode('utf8')
         with open("c:\\douban_download\\"+str(j)+name.decode('utf8'),'wb') as jpg:
             jpg.write(img)
+            i = i + 1
             j = j + 1
     return
 
@@ -63,6 +83,20 @@ for author in getAuthor(html):
     numOfAuthor = numOfAuthor + 1
     print author.decode('utf8'),numOfAuthor
 
+
+numOfTranslator = 0
+for Translator in getTranslator(html):
+    numOfTranslator = numOfTranslator + 1
+    if type(Translator) == list:
+        for j in Translator:
+            print j.decode('utf8'),
+        print numOfTranslator
+    else:
+        print Translator,numOfTranslator
+
+
+html = replaceAuthor(html)
+
 getCover(html,getName(html))
 
 numOfClass = 0
@@ -74,4 +108,3 @@ numOfIntro = 0
 for BookIntroduction in getIntroduction(html):
     numOfIntro = numOfIntro + 1
     print BookIntroduction.decode('utf8'),numOfIntro
-
